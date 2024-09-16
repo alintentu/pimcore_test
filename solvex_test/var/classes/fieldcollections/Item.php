a69d676f9d9389cb80d0b139f9216c405f4854b0
Alin Tentu <alin.tentu@ymail.com>
2024-09-16 15:05:33 +0300

    first commit

---
+++ solvex_test/var/classes/fieldcollections/Item.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Fields Summary:
+ */
+
+return \Pimcore\Model\DataObject\Fieldcollection\Definition::__set_state(array(
+   'dao' => NULL,
+   'key' => 'Item',
+   'parentClass' => NULL,
+   'implementsInterfaces' => NULL,
+   'title' => NULL,
+   'group' => NULL,
+   'layoutDefinitions' => NULL,
+   'fieldDefinitionsCache' => NULL,
+   'blockedVarsForExport' => 
+  array (
+  ),
+   'activeDispatchingEvents' => 
+  array (
+  ),
+));
---
+++ solvex_test/var/classes/objectbricks/Item.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Fields Summary:
+ */
+
+return \Pimcore\Model\DataObject\Objectbrick\Definition::__set_state(array(
+   'dao' => NULL,
+   'key' => 'Item',
+   'parentClass' => NULL,
+   'implementsInterfaces' => NULL,
+   'title' => NULL,
+   'group' => NULL,
+   'layoutDefinitions' => NULL,
+   'fieldDefinitionsCache' => NULL,
+   'blockedVarsForExport' => 
+  array (
+  ),
+   'activeDispatchingEvents' => 
+  array (
+  ),
+   'classDefinitions' => 
+  array (
+  ),
+   'activeDispatchingEvents' => 
+  array (
+  ),
+));
